106d459923c44dc5c816a9a6aeb5dc8751ace304
direstsprccedpaed-boop <direst.spr.cced.paed@gmail.com>
2026-08-31 09:24:12 +0200

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -15,7 +15,6 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
@@ -23,7 +22,7 @@ if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 
 if defined JAVA_HOME goto findJavaFromJavaHome
 
